4af06d6e23ee9048e89d70c797e3ef25fb7be469
takeuchi <takeuchi@mfac.jp>
2016-04-30 14:53:02 +0900

    一定時間でコインが消える

---
+++ Assets/chipDelete.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class chipDelete : MonoBehaviour {
+
+	public float life_time = 4f;
+	float time = 0f;
+
+	// Use this for initialization
+	void Start () {
+		print (Time.deltaTime);
+		print (time);
+		time = 0f;
+		life_time = (4f * Random.value) + 4f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		print ("life_time------>" + life_time);
+		if(time>life_time){
+			//time = 0f;
+			Destroy(gameObject);
+		}
+		time = time + Time.deltaTime;
+	}
+}
--- Assets/chipSpawn.cs
+++ Assets/chipSpawn.cs
@@ -7,10 +7,10 @@ public class chipSpawn : MonoBehaviour {
 	public float intervalSecond = 1.0f;
 	//public float randomOffsetY = 5f;
 
+
 	// Use this for initialization
 	void Start () {
 		StartCoroutine("Spawn");
-	
 	}
 	
 	IEnumerator Spawn() {
@@ -22,26 +22,6 @@ public class chipSpawn : MonoBehaviour {
 			yield return new WaitForSeconds(intervalSecond);
 		}
 	}
+
 }
-
-/*using UnityEngine;
-using System.Collections;
-
-public class IntervalSpawner : MonoBehaviour {
-	public GameObject prefab;
-	public float intervalSecond = 1.0f;
-
-	// Use this for initialization
-	void Start () {
-		StartCoroutine("Spawn");
-	}
-
-	IEnumerator Spawn() {
-		while(true) {
-			Instantiate(prefab, this.transform.position, prefab.transform.rotation);
-			yield return new WaitForSeconds(intervalSecond);
-		}
-	}
-}
-Status API Training Shop Blog About
-*/
+	
