667438e44b6908cae313de0f1854687be5f890e3
takeuchi <takeuchi@mfac.jp>
2016-05-02 00:52:09 +0900

    検証中

---
+++ singleSlot/Assets/moveY.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveY : MonoBehaviour {
+
+	// Use this for initialization
+	///void Start () {
+	
+	//}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3
+			(
+			transform.position.x, 
+			Mathf.PingPong(Time.time,5), 
+			transform.position.z
+		);
+	}
+}
