b67e1d9d0a1804c63eeda8019578e24b737fa4e6
takeuchi <takeuchi@mfac.jp>
2016-05-03 09:37:18 +0900

    y座標

--- singleSlot/Assets/moveY.cs
+++ singleSlot/Assets/moveY.cs
@@ -9,12 +9,23 @@ public class moveY : MonoBehaviour {
 	//}
 	
 	// Update is called once per frame
+	public float y;
+
 	void Update () {
+		y --;
 		transform.position = new Vector3
 			(
 			transform.position.x, 
-			Mathf.PingPong(Time.time,5), 
+			//Mathf.PingPong(Time.time,5), 
+			y, 
 			transform.position.z
 		);
+		print("Time.time----->" + Time.time);
+		print(Mathf.PingPong(Time.time,5));
+
+		if(transform.position.y < -6){
+			y = 6;
+		}
+
 	}
 }
