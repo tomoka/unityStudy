512e48c036f15e6f47b9c01104921eb6ea7dfbad
takeuchi <takeuchi@mfac.jp>
2016-05-18 02:23:16 +0900

    スロット判別追加

--- singleSlot/Assets/sample.cs
+++ singleSlot/Assets/sample.cs
@@ -3,19 +3,19 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class sample : MonoBehaviour {
-	RectTransform rectTransform = null;
+	//RectTransform rectTransform = null;
 	//Transform Transform = null;
 
 	// Use this for initialization
 	void Start () {
-		rectTransform = GetComponent<RectTransform> ();
+		//rectTransform = GetComponent<RectTransform> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log("rectTransform.localPosition--->" + (this.rectTransform.localPosition));
-		Debug.Log("rectTransform.rect--->" + (this.rectTransform.rect));
-		Debug.Log("rectTransform.position--->" + (this.rectTransform.position));
+		///Debug.Log("rectTransform.localPosition--->" + (this.rectTransform.localPosition));
+		//Debug.Log("rectTransform.rect--->" + (this.rectTransform.rect));
+		//Debug.Log("rectTransform.position--->" + (this.rectTransform.position));
 
 	}
 }
